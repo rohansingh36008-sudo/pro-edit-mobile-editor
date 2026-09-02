b8dd14f42378be15d137f878f73162099c928942
rohansingh36008-sudo <rohansingh36008@gmail.com>
2026-09-02 13:17:16 +0530

    Add app/(tabs)/_layout.tsx

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Tabs } from 'expo-router';
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: { display: 'none' },
+      }}
+    />
+  );
+}
